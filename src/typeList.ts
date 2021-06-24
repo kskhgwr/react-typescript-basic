3adef3be72d05baf6daebc60e890d31e8f009e5f
kskhgwr <77972033+kskhgwr@users.noreply.github.com>
2021-06-24 22:34:59 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars  */
+
+/**
+ * Typescriptの基本の型
+ */
+
+// boolean
+let bool: boolean = true;
+
+// number　数値
+let num: number = 0;
+
+// string 文字列
+let str: string = "A";
+
+// Array 配列
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = () => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let nundefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
